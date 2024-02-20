7e12dbf7175f8f7541480c2ae082b8b6629644b8
kimgoat <kimgoatlove@gmail.com>
2024-02-20 17:32:30 +0900

    MergeSort

--- sort/MergeSort.java
+++ sort/MergeSort.java
@@ -1,6 +1,41 @@
 package sort;
 
 public class MergeSort {
+    static int[] buff;
+
+    static void mergeSort(int[] a, int left, int right) {
+        if (left < right) {
+            int i;
+            int center = (left + right) / 2;
+            int p = 0;
+            int j = 0;
+            int k = left;
+
+            mergeSort(a, left, center);
+            mergeSort(a, center + 1, right);
+
+            for (i = left; i <= center; i++)
+                buff[p++] = a[i];
+
+            while (i <= right && j < p)
+                a[k++] = (buff[j] <= a[i]) ? buff[j++] : a[i++];
+
+            while (j < p)
+                a[k++] = buff[j++];
+
+        }
+    }
+
+    static void mergeSort(int[] a, int n) {
+        buff = new int[n];
+        mergeSort(a, 0, n - 1);
+        buff = null;
+    }
+
+    static void mergeSort(int[] a) {
+        mergeSort(a, a.length);
+    }
+
     static void mergeSort(int[] a, int[] b, int[] c) {
         mergeSort(a, a.length, b, b.length, c, c.length);
     }
@@ -41,6 +76,14 @@ public class MergeSort {
         System.out.println("----merge----");
         mergeSort(a, b, c);
         printArr(c);
+        System.out.println();
+
+        int[] d = { 22, 5, 11, 32, 120, 68, 70 };
+        System.out.println("Before Merge : Array d");
+        printArr(d);
+        mergeSort(d);
+        System.out.println("After Merge : Array d");
+        printArr(d);
 
     }
 }
